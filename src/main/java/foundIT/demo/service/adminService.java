7c415a4a268679639f1a1b8234b3fe8442652db6
SmithPatrick6000c <patdousmi@gmail.com>
2026-03-21 16:22:56 -0400

    No Admin Duplicates

--- src/main/java/foundIT/demo/service/adminService.java
+++ src/main/java/foundIT/demo/service/adminService.java
@@ -8,7 +8,9 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import foundIT.demo.model.admin;
 
@@ -31,6 +33,10 @@ public class adminService
 
     public admin createAdmin(admin ad)
     {
+        if(usernameExists(ad))
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Choose another Username");
+        }
         return mongoTemplate.save(ad);
     }
 
@@ -55,12 +61,16 @@ public class adminService
 
     public admin updateAdmin(String id, admin admin)
     {
+        if(usernameExists(admin))
+        {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Choose another Username");
+        }
         var query = new Query(Criteria.where("id").is(id));
 
         Update updatedAdmin = new Update()
                         .set("username", admin.getUsername())
                         .set("password", admin.getPassword())
-                         .set("firstName", admin.getFirstName())
+                        .set("firstName", admin.getFirstName())
                         .set("lastName", admin.getLastName());        
 
         mongoTemplate.updateFirst(query, updatedAdmin, admin.class);
@@ -74,6 +84,20 @@ public class adminService
         var query = new Query(Criteria.where("id").is(id));
 
         return mongoTemplate.remove(query, admin.class).getDeletedCount() > 0;
+    }
+
+
+    public boolean usernameExists(admin ia)
+    {
+        List<admin> la = getAllAdmins();
+        for ( admin a : la)
+        {
+            if (a.getUsername().equals(ia.getUsername()))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
 
