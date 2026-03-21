c3386de68e58aa0edefb0e20305c3bcebe587778
LRomero01 <romerofernandezlc@appstate.edu>
2026-03-21 12:46:39 -0400

    Admin model

--- src/main/java/foundIT/demo/model/admin.java
+++ src/main/java/foundIT/demo/model/admin.java
@@ -25,5 +25,52 @@ public class admin {
     private String password;
 
 
+    public admin(String username, String password, String firstName, String lastName)
+    {
+     this.username = username;
+     this.password = password;
+     this.firstName = firstName;
+     this.lastName = lastName;
+    }
 
+    public String getUsername()
+    {
+        return this.username;
+    }
+
+    public void setUsername(String un)
+    {
+        this.username = un;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getPW ()
+    {
+        return this.password;
+    }
+
+    public String getFirstName ()
+    {
+        return this.firstName;
+    }
+
+    public String lastName()
+    {
+        return this.lastName;
+    }
+
+    public void setFN(String fn)
+    {
+        this.firstName = fn;
+    }
+
+    public void setLN(String ln)
+    {
+        this.lastName = ln;
+    }
 }
+
