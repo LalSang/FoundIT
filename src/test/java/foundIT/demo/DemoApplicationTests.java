b3a9f5f5e69aa3e80914236c0a9b5d7922d7b8bb
SmithPatrick6000c <patdousmi@gmail.com>
2026-03-21 12:05:15 -0400

    first commit

---
+++ src/test/java/foundIT/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package foundIT.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
