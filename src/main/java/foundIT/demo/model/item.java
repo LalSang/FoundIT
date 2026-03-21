223538540849576dd4ed7c3663c58a94f27b6a8b
SmithPatrick6000c <patdousmi@gmail.com>
2026-03-21 12:41:44 -0400

    Getters/Setters for Item and FoundItem

--- src/main/java/foundIT/demo/model/item.java
+++ src/main/java/foundIT/demo/model/item.java
@@ -20,15 +20,67 @@ public class item {
     private String itemType;
 
     @NotBlank(message = "Description is Required")
-    private String description;
+    private String desc;
 
     @NotBlank(message = "Location is Required")
-    private String location;
+    private String loc;
 
     @NotBlank(message = "Date is Required")
     private String date;
 
 
-    
+    public item (String adminId, String itemType, String desc, String loc, String date)
+    {
+        this.adminId = adminId;
+        this.itemType = itemType;
+        this.desc = desc;
+        this.loc = loc;
+        this.date = date;
+    }
+
+    public String getAdminId()
+    {
+        return this.adminId;
+    }
+    public void setAdminId(String adminId)
+    {
+        this.adminId = adminId;
+    }
+
+    public String getItemType()
+    {
+        return this.itemType;
+    }
+    public void setItemType(String itemType)
+    {
+        this.itemType = itemType;
+    }
+
+    public String getDesc()
+    {
+        return this.desc;
+    }
+    public void setDesc(String desc)
+    {
+        this.desc = desc;
+    }
+
+    public String getLoc()
+    {
+        return this.loc;
+    }
+    public void setLoc(String loc)
+    {
+        this.loc = loc;
+    }
+
+    public String getDate()
+    {
+        return this.date;
+    }
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
 
 }
