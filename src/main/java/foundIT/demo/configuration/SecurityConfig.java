4b6378cc20e21e31b1ed1a1e401bfa0ae32d427f
LRomero01 <romerofernandezlc@appstate.edu>
2026-03-21 17:50:00 -0400

    something

--- src/main/java/foundIT/demo/configuration/SecurityConfig.java
+++ src/main/java/foundIT/demo/configuration/SecurityConfig.java
@@ -2,14 +2,33 @@ package foundIT.demo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    
+    @Bean
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+    .csrf(csrf -> csrf.disable())
+    .httpBasic(httpBasic -> httpBasic.disable())
+    .formLogin(form -> form.disable())
+    .authorizeHttpRequests(auth -> auth
+        .anyRequest().permitAll()
+    );
+
+        return http.build();
     }
 }
